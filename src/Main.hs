a192852362d9ce2d042ef74bd7167f908a9868a7
Vladislav Skukov <v.skukov@gmail.com>
2016-10-22 01:07:59 +0300

    Added expression parsers

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,9 @@ module Main where
 import System.IO
 import System.Environment
 import Control.Monad
-import Text.ParserCombinators.Parsec hiding (spaces)
+import Text.ParserCombinators.Parsec
 
 {- Defining ADT for contracts -}
-type Type = String
-
 data Variable = Variable String String
                 deriving (Show)
 
@@ -32,13 +30,13 @@ data Rule = Rule RuleName Variables Preconds Body Postconds
             deriving (Show)
 
 ruleExample :: String
-ruleExample = "rule_of_withdrawal (ac: ACCOUNT; n, initial_amount: INTEGER;): \
+ruleExample = "rule_of_withdrawal (ac: ACCOUNT; n, initial_amount: INTEGER): \
        \ {ac.amount = initial_amount; ac.amount <= n} ac.withdraw (n) {ac.amount = initial_amount - n}"
 
 {- Defining parsers -}
 
 symbol :: Parser Char
-symbol = oneOf "-_!?"
+symbol = oneOf ".-_!?"
 
 (<<) :: Parser a -> Parser b -> Parser a
 p << q = do
@@ -79,8 +77,48 @@ variables = do
   char ')'
   return $ concat vars
 
---parseRule :: Parser Rule
---parseRule =
+eVar :: Parser Expr
+eVar = do
+  var <- varString
+  return $ EVar var
+
+exprString :: Parser String
+exprString = many $ letter <|> digit <|> symbol
+
+operator :: Parser String
+operator =  string "+"
+        <|> string "-"
+        <|> string "*"
+        <|> string "/"
+        <|> string "^"
+        <|> string "**"
+        <|> string "div"
+        <|> string "mod"
+
+eBinary :: Parser Expr
+eBinary = do
+  var1 <- exprString
+  many space
+  op <- operator
+  many space
+  var2 <- exprString
+  return $ EBinary var1 op var2
+
+eCall :: Parser Expr
+eCall = do
+  var <- varString
+  many space
+  char '('
+  vars <- sepBy (around varString) (comma)
+  char ')'
+  return $ ECall var vars
+
+expression :: Parser Expr
+expression = try eCall <|> eVar <|> eBinary
+
+
+--rule :: Parser Rule
+--rule = do
 
 
 readExpr :: String -> String
